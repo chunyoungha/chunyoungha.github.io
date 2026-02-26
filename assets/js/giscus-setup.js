60ba199c8fe1d20f2eec94f5dd30f525d37de77b
chunyoungha <arniechicken@naver.com>
2026-02-26 13:47:25 +0000

    Deploying to gh-pages from @ chunyoungha/chunyoungha.github.io@d9ab964eae2438f3d054d184248040fc8634a56c 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "chunyoungha/ychunlab",
+    "data-repo": "chunyoungha/chunyoungha.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
